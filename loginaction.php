de54cea10a826dc3056119a5a206db9ad1674b36
Renan <renanavs@hotmail.com>
2018-12-18 16:53:57 -0200

    dev sistema login 1 etapa do projeto

---
+++ loginaction.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include 'connection.php';
+
+global $login;
+global $password;
+
+$login = $_POST['login'];
+$password = $_POST['password'];
+
+$sql = "SELECT id, nome FROM associado WHERE login = '$login' AND senha = '$password'";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+if($row){
+    $_SESSION['id'] = $row['id'];
+	$_SESSION['nome'] = $row['nome'];
+    header("location: google.com");
+}else{
+	echo "<script> alert('Senha ou Login incorreto');</script>";
+}
+?>
