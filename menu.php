34a75569d11ecd0f44e8737e6099b2b7f4353780
Renan <renanavs@hotmail.com>
2019-01-10 01:53:57 -0200

    inciando o desenvolvimento do menu

---
+++ menu.php
@@ -0,0 +1,24 @@
+<?php
+	// if(!$_SESSION['id']){
+	// 	session_destroy();
+	// 	header("Location: index.html");
+	// }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Menu</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/menu.css">
+	<script src="js/jquery-3.3.1.js"></script>
+</head>
+<body>
+	<?php include 'navbar.html'; ?>
+	<br>
+	<br>
+	<center>
+		<a class="btn btn-secondary" href="associado.php">Associado</a>
+	</center>
+</body>
+</html>
