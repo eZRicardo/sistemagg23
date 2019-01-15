6732cdf281d9f381aa42d35cf8359dc586ba2c20
Renan <renanavs@hotmail.com>
2019-01-14 23:51:30 -0200

    ajuste do uso de tables

--- loginaction.php
+++ loginaction.php
@@ -8,7 +8,7 @@ global $password;
 $login = $_POST['login'];
 $password = $_POST['password'];
 
-$sql = "SELECT id, nome FROM associado WHERE login = '$login' AND senha = '$password'";
+$sql = "SELECT id, nome FROM usuario WHERE login = '$login' AND senha = '$password'";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
 if($row){
